890887ceebb317b8cd548d3b0126d4eee0d6acfd
Nayanalekha <38463625+Nayanalekha@users.noreply.github.com>
2022-06-06 13:38:54 +1200

    lambda expression calculator

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    public static void main(String[] args) {
+        Calculater sum = (a,b) -> a + b;
+        Calculater multiply = (a,b) -> a * b;
+        calculate(5,7, sum);
+        calculate(5,7, multiply);
+    }
+    public static void calculate(int a,int b, Calculater format) {
+        int result = format.run(a,b);
+        System.out.println(result);
+    }
+}
+interface Calculater {
+    int run(int a, int b);
+}
